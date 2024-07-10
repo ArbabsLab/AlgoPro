25813e902dc2cda05129dd4d879a5b759147d844
Arbab Husain <arbabhusain200@gmail.com>
2024-07-09 22:27:26 -0400

    Added add function to binary tree. Lowkey proud of this one.

---
+++ BinaryTree.js
@@ -0,0 +1,57 @@
+function Node(ele){
+    this.val = ele;
+    this.right = null;
+    this.left = null;
+
+    this.getVal = () => {
+        return this.val;
+    }
+}
+
+function BinaryTree(){
+    this.root = null;
+
+    this.add = (ele) => {
+        if (this.root == null){
+            node = new Node(ele);
+            this.root = node;
+        }
+        else{
+            dummyRoot = this.root;
+            node = new Node(ele);
+            while(true){
+                if (ele < dummyRoot.getVal()){
+                    if (dummyRoot.left == null){
+                        dummyRoot.left = node;
+                        break;
+                    }
+                    else{
+                        dummyRoot = dummyRoot.left;
+                        continue;
+                    }
+                }
+                else{
+                    if (dummyRoot.right == null){
+                        dummyRoot.right = node;
+                        break;
+                    }
+                    else{
+                        dummyRoot = dummyRoot.right;
+                        continue;
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+test = new BinaryTree()
+test.add(2)
+test.add(1)
+test.add(4)
+test.add(5)
+test.add(3)
+test.add(-1)
+
+console.log(test)
--- Tree.js
+++
@@ -1,9 +0,0 @@
-function Node(ele){
-    this.val = ele;
-    this.right = null;
-    this.left = null;
-
-    this.getVal = () => {
-        return this.val;
-    }
-}
