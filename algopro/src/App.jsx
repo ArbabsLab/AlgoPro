90eb2ecc8f0240a0fb23d7254a10f40e2246b548
Arbab Husain <arbabhusain200@gmail.com>
2024-07-15 22:14:12 -0400

    Created React project

---
+++ algopro/src/App.jsx
@@ -0,0 +1,15 @@
+
+import NodeCard from "./components/NodeCard"
+import NodeArrow from "./components/NodeArrow"
+
+function App() {
+  
+  return (
+    <main>
+      <NodeCard />
+      <NodeArrow />
+    </main>
+  )
+}
+
+export default App
