bff692c5c27b3b3d9571f985a6ab9da00a24bf56
Arbab Husain <arbabhusain200@gmail.com>
2024-07-20 19:30:26 -0400

    Added sort and traverse page routes

---
+++ algopro/src/components/SortPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SortPage() {
+  return (
+    <h1>Sort Page</h1>
+  )
+}
---
+++ algopro/src/components/TraversePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TraversePage() {
+  return (
+    <h1>Traverse Page</h1>
+  )
+}
