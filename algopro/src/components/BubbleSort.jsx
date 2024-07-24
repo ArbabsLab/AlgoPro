276473ecb15f742120084d9c01ba7391f72c5cb0
Arbab Husain <arbabhusain200@gmail.com>
2024-07-23 20:25:59 -0400

    Added bubblesort visualization

--- algopro/src/components/BubbleSort.jsx
+++ algopro/src/components/BubbleSort.jsx
@@ -5,7 +5,7 @@ export default function BubbleSort() {
     const maxNum = 100
     const minNum = 0
 
-    const [node, setNode] = useState(0)
+    const [node, setNode] = useState("")
     const [data, setData] = useState([])
 
     const nodeChange = (event) => {
@@ -14,8 +14,10 @@ export default function BubbleSort() {
     }
 
     const dataSubmit = () => {
-        setData([...data, node])
-        setNode(0)
+        
+        setData([...data, Number(node)]); 
+        
+        setNode("")
     }
 
     const resetData = () => {
@@ -23,13 +25,47 @@ export default function BubbleSort() {
         console.log(data)
     }
 
+    
+    const bubbleSort = () => {
+        const arr = [...data];
+        let n = arr.length;
+        let swapped;
+
+        do {
+            swapped = false;
+            for (let i = 0; i < n - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    let temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+            n--; 
+        } while (swapped);
+
+        setData(arr);
+        console.log(arr); // Log the sorted array
+    };
+
+    const insertionSort = () => {
+
+    }
+
+    const quickSort = () => {
+
+    }
+
+    const mergeSort = () => {
+
+    }
    
 
     return (
         <>
         <div className="prelim-sort-buttons">
             <label for='insert-node'>Add Number({minNum} - {maxNum})</label>
-            <input type="number" value={node} name='insert-node' onChange={nodeChange}></input>
+            <input type="text" value={node} name='insert-node' onChange={nodeChange}></input>
             <button className='sort-btn' onClick={dataSubmit}>Add</button>
 
             <button className='sort-btn' onClick={resetData}>Reset</button>
@@ -37,10 +73,10 @@ export default function BubbleSort() {
         
         <div className='container-sort'>
             {
-                data.map((num, idx) => 
+                data.map((num) => 
                     <div className='data-bar'>
                     <h3>{num}</h3>
-                    <div key={idx} style={{
+                    <div style={{
                         display: 'flex',
                         background:'#22223b',
                         width: '25px',
@@ -51,12 +87,17 @@ export default function BubbleSort() {
                     }}
                     ></div>
                     </div>
-                    
-                    
                 )
             }
-            
         </div>
+
+        <div className="alg-btn-sort">
+            <button className='sort-btn' onClick={bubbleSort}>Bubble Sort</button>
+            <button className='sort-btn' onClick={insertionSort}>Insertion Sort</button>
+            <button className='sort-btn' onClick={quickSort}>Quick Sort</button>
+            <button className='sort-btn' onClick={mergeSort}>Merge Sort</button>
+        </div>
+        
         </>
         
     )
